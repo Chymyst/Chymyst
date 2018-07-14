ddd4faf1433d866a350304a3b06fa80302615ce5
Sergei Winitzki <winitzki@gmail.com>
2018-07-14 02:16:21 -0700

    improve presentation

--- src/test/scala/io/chymyst/lab/ChymystPresentation.scala
+++ src/test/scala/io/chymyst/lab/ChymystPresentation.scala
@@ -94,23 +94,24 @@ class ChymystPresentation extends FlatSpec with Matchers {
 
   We would like to rewrite Example 1 like this:
 
-  - declare `plus()` and `times()` as concurrent functions
-  - make (1, 2) and (3, 4) available as concurrent data
+  - declare `plus()` and `times()` as "concurrent functions"
+  - make the tuples (1, 2) and (3, 4) available as "concurrent data"
   - somehow say that these are actually inputs for two parallel runs of `plus`
-  - somehow say that `times` should get its input from the results of `plus`
+  - somehow say that `times` should get its input from the two results of `plus`
 
-  Imagine we could write (in a modified Scala):
+  Imagine we could write (in a somehow modified Scala):
 
   def_concur plus(inp: (Int, Int)) = ... // Defines `plus()` as concurrent function.
 
-  def emit(x: _) = ...  // Makes `x` somehow available as "concurrent data".
+  def emit(x: Any) = ...  // Makes `x` somehow available as "concurrent data".
 
   emit((1, 2)) // Make the tuple (1, 2) available as concurrent data.
   emit((3, 4)) // Make the tuple (3, 4) available.
 
   How can we specify that the tuple (1, 2) is intended as input for `plus`?
-  We do not directly write `plus((1, 2))`: We already declared `plus` as concurrent,
-  so now `plus` should somehow consume its input data automatically.
+  We do not want to write `plus((1, 2))` and `plus((3, 4))` explicitly:
+  We already declared `plus()` as a concurrent function, and that should be enough.
+  So now `plus` should somehow find and consume its input data automatically.
 
   It is clear that we have to _label_ the concurrent data in some way, in order
   to express the connection between concurrent data and the declared functions.
@@ -118,7 +119,9 @@ class ChymystPresentation extends FlatSpec with Matchers {
   Let us attach the name `inp` as a label for the concurrent data for `plus()`.
   The name `inp` corresponds to the name of the input variable in `plus()`.
   
-  Instead of `emit((1, 2))`, we will have to say `emit("inp", (1, 2))`.
+  Instead of `emit((1, 2))`, we will have to say:
+
+    emit("inp", (1, 2)) // Make (1, 2) available as concurrent data labeled "inp".
 
   This seems to be workable now. We can implement a runtime system that will start
   a new concurrent run of `plus()` automatically on a new thread, as soon as a new
@@ -131,7 +134,7 @@ class ChymystPresentation extends FlatSpec with Matchers {
   First, we need to make sure it is not mixed up with the input for `plus()`.
   When we define `times`, we will use another name for its input value, say `inp2`:
 
-  def_concur times(inp2: (Int, Int)): Int = ... // Use `inp2` as input name.
+  def_concur times(inp2: (Int, Int)) = ... // Use `inp2` as input name.
 
   However, the problem remains that we need to specify that `inp2` is made up from
   the two results obtained after two parallel runs of `plus()`.
@@ -152,12 +155,12 @@ class ChymystPresentation extends FlatSpec with Matchers {
 
   Let us then redefine the concurrent functions `plus()` and `times()` like this:
 
-  def_concur plus(inp: (Label, Int, Int)): Unit = {
+  def_concur plus(inp: (Label, Int, Int)) = {
     val (output_label, x, y) = inp
     emit(output_label, x + y)
   }
 
-  def_concur times(inp1: Int, inp2: Int): Unit = println(inp1 * inp2)
+  def_concur times(inp1: Int, inp2: Int) = println(inp1 * inp2)
 
   emit("inp", ("inp1", 1, 2))
   emit("inp", ("inp2", 3, 4))
@@ -168,16 +171,16 @@ class ChymystPresentation extends FlatSpec with Matchers {
   both of these values become available (i.e. are emitted), the computation `times`
   is run and prints the result.
 
-  What we have achieved with this DSL design:
+  Here is what we have achieved with this DSL design so far:
 
   - we can declare functions to be "concurrent"
   - we can declare data to be "concurrent", if we provide a suitable "label"
   - concurrent functions will automatically find and consume their inputs
-  - concurrency and parallelism are automatic and data-driven
+  - concurrency and parallelism are automatic and "data-driven":
   - if multiple input data is available, multiple runs of concurrent functions
       are automatically started in parallel
   - we do not need to write any code that starts parallel computations explicitly
-  - the runtime system will take care of scheduling and create threads as needed
+  - the runtime system can take care of scheduling and create threads as needed
 
   Let us now simplify the DSL somewhat and strengthen the type usage.
 
@@ -188,13 +191,13 @@ class ChymystPresentation extends FlatSpec with Matchers {
     the type `A` of the data item. (The `Chymyst` library defines this class.)
   A label of type `M[Int]` must be used for data items of type `Int`, for example.
 
-  - To define labels, we could use the constructors of the class `M`:
+  - To define labels, we use the constructor of the class `M`:
   */
   val inp1 = new M[Int]("input 1") // Names for debugging purposes only.
   val inp2 = new M[Int]("input 2")
   /*
   - To emit concurrent data, write simply `inp1(7)` instead of `emit("inp1", 7)`.
-  - This is implemented via `M#apply()`.
+  This is implemented via `M#apply()` in the `Chymyst` library.
 
   - To define concurrent functions, we use the `go()` method and a special syntax,
   as defined by the `Chymyst` library.
@@ -203,33 +206,38 @@ class ChymystPresentation extends FlatSpec with Matchers {
   */
   val times = go { case inp1(x) + inp2(y) ⇒ println(x * y) }
   /*
-  In this syntax, `inp1(x)` means "the concurrent data value x, labeled by inp1`.
+  In this syntax, `inp1(x)` means "the concurrent data value x, labeled by inp1".
 
   Note that the labels `inp1` and `inp2` have been already defined to carry
   values of type `Int`. Here, we use these labels in a pattern-matching syntax.
-  Type checking of pattern variables is enforced in the standard Scala manner.
+  Type checking of pattern variables `x`, `y` is enforced as usual in Scala code.
 
-  - Now define our `plus` function and its input data label, `inp`:
+  - Define the concurrent function `plus` and its input data label, `inp`:
   */
   val inp = new M[(M[Int], Int, Int)]("input for plus")
 
   val plus = go { case inp((outputLabel, x, y)) ⇒ outputLabel(x + y) }
   /*
-  Now, the concurrent functions `plus` and `times` are declaratively defined as
-  values. We need to tell the runtime system somehow to "activate" them, so that
-  the runtime will begin to wait for their input data.
+  So far, we have defined the labels `inp`, `inp1`, `inp2` and the concurrent
+  functions `plus` and `times` as local values. These definitions are merely
+  descriptions of the concurrent logic of our program; they do not cause any
+  threads to be created or any computations to start.
+
+  Before we can use this program (by emitting some data items with label `inp`),
+  we need to tell the runtime system to "activate" our definitions. Once this is
+  done, the runtime will begin to monitor input data items that are emitted.
 
   It is convenient to make the "activation" into a separate step in the DSL.
 
-  The activation method is called `site()` that takes a list of declared functions:
+  The activation method is called `site()` and takes a list of declared functions:
   */
   site(plus, times)
   /*
-  The motivation for the name `site` is that we are defining a "holding site" for
+  The motivation for the name `site` is that we need to define a "holding site" for
   concurrent data, where the emitted data items with labels `inp`, `inp1`, `inp2`
-  will gather and wait to be consumed by concurrent functions we declared.
+  will gather, waiting to be consumed by concurrent functions we declared.
 
-  Let's check whether this actually works:
+  Let's check whether this code actually works:
   */
   it should "run the concurrent functions defined using new DSL" in {
     // Emit the required concurrent input data.
@@ -239,16 +247,19 @@ class ChymystPresentation extends FlatSpec with Matchers {
   }
 
   /*
-  Example 2: A computation that processes arriving data items concurrently.
+  Example 2: A computation that processes all arriving data items concurrently.
 
-  Suppose we expect data items carrying `Double` values x_1, x_2, ...
+  Suppose we expect to receive data items carrying `Double` values: x_1, x_2, ...
   Each time, we compute `cos(x_i)` and then update the running max value `max_cos`.
   We would like to compute all `cos(x_i)` in parallel, allowing `max_cos` to be
   updated in arbitrary order.
 
-  We model x_1, x_2, ... as "concurrent data" inputs. They need a label: say, `inp`.
-  They can share the same label, because the computation `cos(x_i)` is the same
-  and needs to be run in parallel.
+  We model x_1, x_2, ... as "concurrent data" inputs. We assume that they will be
+  emitted by some other code.
+
+  The input concurrent data needs a label: say, `x_in`. It makes sense to use
+  the same label for all x_i, because the computation `cos(x_i)` is the same
+  and needs to be run in parallel on each input data item.
   */
   it should "run example 2" in {
     val x_in = new M[Double]("x_i input")
@@ -263,7 +274,13 @@ class ChymystPresentation extends FlatSpec with Matchers {
     val do_cos = go { case x_in(x) ⇒ x_out(math.cos(x)) }
     /*
     As the `x_in` items arrive, we will consume them and emit `x_out` items.
-    We need to update `max_cos` concurrently, but how?
+    Since all input items have the same label `x_in`, the processing will be
+    automatically parallelized.
+
+    The output values are emitted asynchronously, as concurrent data with
+    the `x_out` label.
+
+    Next, we need to update `max_cos` concurrently and asynchronously. How?
 
     It seems that `max_cos` itself needs to be declared "concurrent data" as well.
     Let's define a label `max_cos` for it, and emit an initial `max_cos` value.
@@ -292,6 +309,10 @@ class ChymystPresentation extends FlatSpec with Matchers {
     */
     val update_max = go { case x_out(x) + max_cos(c) ⇒ max_cos(math.max(x, c)) }
     /*
+    This will work; we are finished with the main code. Now, at any time,
+    the concurrent value with label `max_cos` can be updated. The code is concise
+    (5 lines) and obviously has no deadlocks or race conditions.
+
     It would be useful if we could read the current value of `max_cos` at any time.
 
     However, `max_cos` values are concurrent data, and cannot be directly accessed.
@@ -306,7 +327,7 @@ class ChymystPresentation extends FlatSpec with Matchers {
     is emitted. This is clearly not what we want; we need to run that function
     only when we actually need to read the `max_cos` value.
 
-    How will we signal that we want to read that value? The only way to signal
+    How can we signal that we want to read that value? The only way to "signal"
     anything is to emit an item of concurrent data. Therefore, we need to define
     another label, say `read`, that will be an additional input of the function.
 
@@ -320,14 +341,14 @@ class ChymystPresentation extends FlatSpec with Matchers {
     Let us create an interface between the new concurrent DSL and Scala `Future`s.
 
     We can create a fresh Scala `Promise` and resolve it inside the function body.
-    The `Promise` value can be passed as concurrent data with the `read` label:
+    The `Promise` value will be passed as concurrent data with the `read` label:
      */
     val read = new M[Promise[Double]]("read max_cos")
     val read_max = go { case max_cos(c) + read(p) ⇒ p.success(c) }
     /*
     Now we can emit read() with a fresh `Promise` value, and wait for the result.
 
-    Before testing this code, we need to activate the concurrent site.
+    Before testing this code, we need to activate the "concurrent site".
     The site will hold all concurrent data used as input for any concurrent
     functions we defined.
      */
@@ -337,7 +358,7 @@ class ChymystPresentation extends FlatSpec with Matchers {
     max_cos(-1.0) // Should be safe, since `math.cos()` is never below -1.
     // Emit some x_in values.
     (1 to 10).foreach(x ⇒ x_in(x.toDouble))
-    // Wait for computations to finish. Brittle! But this is just a first test.
+    // Wait for computations to finish. Brittle! But this is just a crude test.
     Thread.sleep(500)
     // Make a fresh `Promise` and emit it as concurrent data with label `read`.
     val pr = Promise[Double]()
@@ -382,6 +403,12 @@ class ChymystPresentation extends FlatSpec with Matchers {
   My experience with the chemical machine shows that, after some practice,
   one can systematically derive safe, declarative, and very concise implementations
   for a large number of concurrency algorithms I have examined.
+
+  Programming in the chemical machine is "systematic" because there are very few
+  primitives and very few ways for the programmer to control the concurrent
+  computation. Usually, there is only one way of solving a given concurrency
+  problem, and one can arrive at the solution by logical reasoning, as I
+  illustrated by the examples in this tutorial.
 
   The chemical machine itself can be embedded as a library-based DSL in many
   programming languages. There exist implementations for OCaml, Haskell, C#, C++,
