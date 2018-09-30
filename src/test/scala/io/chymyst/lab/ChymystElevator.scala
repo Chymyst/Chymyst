11f132c8577ca1f9386fe15f62522460e26ac9de
Sergei Winitzki <winitzki@gmail.com>
2018-09-30 16:45:22 -0700

    implement n-elevator system

--- src/test/scala/io/chymyst/lab/ChymystElevator.scala
+++ src/test/scala/io/chymyst/lab/ChymystElevator.scala
@@ -1,7 +1,10 @@
 package io.chymyst.lab
 
-import org.scalatest.{FlatSpec, Matchers}
 import io.chymyst.jc._
+import org.scalatest.{FlatSpec, Matchers}
+
+import scala.util.Random.nextInt
+
 
 class ChymystElevator extends FlatSpec with Matchers {
 
@@ -47,14 +50,69 @@ class ChymystElevator extends FlatSpec with Matchers {
   type ElevNumber = Int
   type Floor = Int
 
+  case class ElevState(current: Floor = 0, stops: List[Floor] = Nil)
+
+  // Compute the next state of an elevator after one step.
+  val step: ElevState ⇒ ElevState = {
+    case s@ElevState(_, Nil) ⇒ s // Idle elevator.
+    case ElevState(current, next :: rest) ⇒
+      val nextFloor = (next compare current) + current
+      val nextStops = if (nextFloor == next) rest else next :: rest
+      ElevState(nextFloor, nextStops) // Advance to next floor.
+  }
+
+  case class Request(from: Floor, to: Floor, passenger: String)
+
+  // An elevator may accept or refuse a request, depending on the current state.
+
+  // Idle elevators always accept.
+  def idleAccept(r: Request, state: ElevState): ElevState = {
+    state.copy(stops = List(r.from, r.to))
+  }
+
+  // Return the updated elevator state if a request is accepted, otherwise `None`.
+  def canAcceptRequest(r: Request, state: ElevState): Option[ElevState] = {
+    state.stops.headOption match {
+      case None ⇒ // Elevator is idle, accepts requests.
+        Some(idleAccept(r, state))
+      case Some(next) ⇒ // Elevator is at `current` floor, moving towards `next` floor.
+        // The `r.from` floor needs to be between `current` and `next`, not equal to `current`.
+        if ((r.from > state.current && r.from <= next) || (r.from < state.current && r.from >= next))
+          if (r.from == next)
+            Some(state)
+          else Some(state.copy(stops = r.from :: state.stops))
+        else None
+    }
+  }
+
+  // The system decides whether some elevator can accept the request.
+  // The elevator must be idle, or it must be moving towards floor A in the direction of floor B.
+  def choose(r: Request, states: Seq[ElevState]): Option[ElevNumber] = {
+
+    val canAccept: ((ElevState, Int)) ⇒ Boolean = {
+      case (s, _) ⇒ canAcceptRequest(r, s).nonEmpty
+    }
+
+    val distance: ((ElevState, Int)) ⇒ Int = {
+      case (s, _) ⇒ math.abs(s.current - r.from)
+    }
+
+    // Select elevators that are either idle or moving in the correct direction.
+    // From thise, select the closest one.
+    states.zipWithIndex.filter(canAccept).sortBy(distance).headOption.map(_._2)
+  }
+
+  def guidePassenger(r: Request, i: ElevNumber): Unit = {
+    println(s"Passenger ${r.passenger} on floor ${r.from} please proceed to elevator $i")
+  }
+
   it should "run the 3-elevator system" in {
-    case class ElevState(current: Floor = 0, stops: List[Floor] = Nil)
 
     val elev1 = m[ElevState]
     val elev2 = m[ElevState]
     val elev3 = m[ElevState]
 
-    // Define the static reactions. This is just boilerplate.
+    // Define the static reactions.
     val elev1S = go { case _ ⇒ elev1(ElevState()) }
     val elev2S = go { case _ ⇒ elev2(ElevState()) }
     val elev3S = go { case _ ⇒ elev3(ElevState()) }
@@ -66,31 +124,20 @@ class ChymystElevator extends FlatSpec with Matchers {
     val tick2 = m[Unit]
     val tick3 = m[Unit]
 
-    // Compute the next state of an elevator after one step.
-    val step: ElevState ⇒ ElevState = {
-      case s@ElevState(_, Nil) ⇒ s // Idle elevator.
-      case ElevState(current, next :: rest) ⇒
-        val nextFloor = (next compare current) + current
-        val nextStops = if (nextFloor == next) rest else next :: rest
-        ElevState(nextFloor, nextStops) // Advance to next floor.
-    }
-
     // Define reactions for applying one step.
 
     val emitAfter = m[(M[Unit], Long)]
     val emitAfterR = go { case emitAfter((t, d)) ⇒ BlockingIdle(Thread.sleep(d)); t() }
 
     // Elevators may have different speeds.
-    val step1R = go { case elev1(s) + tick1(_) ⇒ elev1(step(s)); emitAfter((tick1, 100L)) }
+    val step1R = go { case elev1(s) + tick1(_) ⇒ elev1(step(s)); emitAfter((tick1, 110L)) }
     val step2R = go { case elev2(s) + tick2(_) ⇒ elev2(step(s)); emitAfter((tick2, 120L)) }
-    val step3R = go { case elev3(s) + tick3(_) ⇒ elev3(step(s)); emitAfter((tick3, 150L)) }
+    val step3R = go { case elev3(s) + tick3(_) ⇒ elev3(step(s)); emitAfter((tick3, 130L)) }
 
     // Each passenger's request, as it is being sent, is modeled by molecule `req`.
     // A delayed request is modeled by `delayed`.
     // A signal for an elevator to accept a request is `accept`.
     // A notification for passenger will be printed to stdout.
-
-    case class Request(from: Floor, to: Floor, passenger: String)
 
     val req = m[Request]
     val delayed = m[Request]
@@ -98,45 +145,6 @@ class ChymystElevator extends FlatSpec with Matchers {
     val accept1 = m[Request]
     val accept2 = m[Request]
     val accept3 = m[Request]
-
-    // An elevator may accept or refuse a request, depending on the current state.
-
-    // Idle elevators always accept.
-    def idleAccept(r: Request, state: ElevState): ElevState = {
-      state.copy(stops = List(r.from, r.to))
-    }
-
-    // Return the updated elevator state if a request is accepted, otherwise `None`.
-    def canAcceptRequest(r: Request, state: ElevState): Option[ElevState] = {
-      state.stops.headOption match {
-        case None ⇒ // Elevator is idle, accepts requests.
-          Some(idleAccept(r, state))
-        case Some(next) ⇒ // Elevator is at `current` floor, moving towards `next` floor.
-          // The `r.from` floor needs to be between `current` and `next`, not equal to `current`.
-          if ((r.from > state.current && r.from <= next) || (r.from < state.current && r.from >= next))
-            if (r.from == next)
-              Some(state)
-            else Some(state.copy(stops = r.from :: state.stops))
-          else None
-      }
-    }
-
-    // The system decides whether some elevator can accept the request.
-    // The elevator must be idle, or it must be moving towards floor A in the direction of floor B.
-    def choose(r: Request, states: Seq[ElevState]): Option[ElevNumber] = {
-
-      val canAccept: ((ElevState, Int)) ⇒ Boolean = {
-        case (s, _) ⇒ canAcceptRequest(r, s).nonEmpty
-      }
-
-      val distance: ((ElevState, Int)) ⇒ Int = {
-        case (s, _) ⇒ math.abs(s.current - r.from)
-      }
-
-      // Select elevators that are either idle or moving in the correct direction.
-      // From thise, select the closest one.
-      states.zipWithIndex.filter(canAccept).sortBy(distance).headOption.map(_._2)
-    }
 
     // A passenger sends a request. An elevator may be chosen and asked to accept the request.
     val reqR = go { case req(r) ⇒
@@ -156,10 +164,6 @@ class ChymystElevator extends FlatSpec with Matchers {
     // Keep track of how many requests still remain unfulfilled.
     val remain = m[Int]
 
-    def guidePassenger(r: Request, i: ElevNumber): Unit = {
-      println(s"Passenger ${r.passenger} on floor ${r.from} please proceed to elevator $i")
-    }
-
     // Request will be refused if the elevator already moved past the requested floor.
     def decideAccept(r: Request, s: ElevState, i: ElevNumber, rem: Int): ElevState = {
       canAcceptRequest(r, s) match {
@@ -172,7 +176,6 @@ class ChymystElevator extends FlatSpec with Matchers {
           req(r) // Try again - perhaps another elevator can accept.
           remain(rem)
           s // Elevator state is unchanged.
-
       }
     }
 
@@ -216,7 +219,6 @@ class ChymystElevator extends FlatSpec with Matchers {
     Seq(tick1, tick2, tick3).foreach(_.apply())
 
     // Passengers send 100 requests at random times.
-    import scala.util.Random.nextInt
     val n = 100
     remain(n)
 
@@ -229,4 +231,110 @@ class ChymystElevator extends FlatSpec with Matchers {
     untilFinished()
   }
 
+  it should "run n-elevator example" in {
+
+    val range = 5 // We have this many elevators.
+
+    val elevs = Seq.fill(range)(m[ElevState])
+
+    // Define the static reactions.
+    val elevS = elevs map (elev ⇒ go { case _ ⇒ elev(ElevState()) })
+
+    // Elevators move floor by floor with fixed speed.
+    // The signal for moving will be modeled by the molecules `tick`.
+
+    val ticks = Seq.fill(range)(m[Unit])
+
+    // Define reactions for applying one step.
+
+    val emitAfter = m[(M[Unit], Long)]
+    val emitAfterR = go { case emitAfter((t, d)) ⇒ BlockingIdle(Thread.sleep(d)); t() }
+
+    // Elevators may have different speeds.
+    val stepsR = (elevs zip ticks).zipWithIndex map { case ((elev, tick), i) ⇒
+      go { case elev(s) + tick(_) ⇒ elev(step(s)); emitAfter((tick, 100L + 10 * i)) }
+    }
+
+    // Each passenger's request, as it is being sent, is modeled by molecule `req`.
+    // A delayed request is modeled by `delayed`.
+    // A signal for an elevator to accept a request is `accept`.
+    // A notification for passenger will be printed to stdout.
+
+    val req = m[Request]
+    val delayed = m[Request]
+
+    val accepts = Seq.fill(range)(m[Request])
+
+    // A passenger sends a request. An elevator may be chosen and asked to accept the request.
+    val reqR = go { case req(r) ⇒
+      // Use volatile readers for elevator states.
+      val states = elevs.map(_.volatileValue)
+      choose(r, states) match {
+        case Some(i) ⇒
+          println(s"Sending an `accept` signal to elevator $i for $r")
+          val accept = accepts(i)
+          accept(r)
+        case None ⇒
+          println(s"No elevator available, $r delayed")
+          delayed(r) // Request delayed, will be accepted later by an idle elevator.
+      }
+    }
+
+    // Keep track of how many requests still remain unfulfilled.
+    val remain = m[Int]
+
+    // Request will be refused if the elevator already moved past the requested floor.
+    def decideAccept(r: Request, s: ElevState, i: ElevNumber, rem: Int): ElevState = {
+      canAcceptRequest(r, s) match {
+        case Some(newState) ⇒
+          guidePassenger(r, i); remain(rem - 1); newState
+
+        case None ⇒
+          // Try again - perhaps another elevator can accept.
+          // Elevator state is unchanged.
+          req(r); remain(rem); s
+      }
+    }
+
+    val accRs = (accepts zip elevs).zipWithIndex map { case ((accept, elev), i) ⇒
+      go { case accept(r) + elev(s) + remain(k) ⇒ elev(decideAccept(r, s, i, k)) }
+    }
+
+    // Delayed requests are served by idle elevators.
+
+    val delayedRs = elevs map { elev ⇒
+      go { case delayed(r) + elev(s@ElevState(_, Nil)) + remain(k) ⇒
+        elev(idleAccept(r, s)); guidePassenger(r, 1); remain(k - 1)
+      }
+    }
+
+    // Signal when all requests are fulfilled and all elevators are idle.
+    val untilFinished = b[Unit, Unit]
+    val idleElevators = m[Int]
+    // Once all requests are served, we compute the number of idle elevators.
+    val allDoneR = go { case remain(0) ⇒ idleElevators(0) }
+    val addIdleR = elevs map (elev ⇒ go { case idleElevators(i) + elev(s@ElevState(_, Nil)) ⇒
+      elev(s); idleElevators(i + 1)
+    })
+    val finishedR = go { case untilFinished(_, reply) + idleElevators(`range`) ⇒ reply() }
+
+    val allReactions = elevS ++ stepsR ++ accRs ++ delayedRs ++ Seq(emitAfterR, reqR, finishedR, allDoneR) ++ addIdleR
+
+    site(allReactions: _*)
+
+    // Run the simulation: emit the initial molecules.
+    ticks.foreach(_.apply())
+
+    // Passengers send 100 requests at random times.
+    val n = 100
+    remain(n)
+
+    (1 to n).foreach { i ⇒
+      Thread.sleep(nextInt(100) + 100L)
+      req(Request(nextInt(20) + 1, nextInt(20) + 1, s"Name-$i"))
+    }
+
+    // Wait until all passenger requests are served.
+    untilFinished()
+  }
 }
